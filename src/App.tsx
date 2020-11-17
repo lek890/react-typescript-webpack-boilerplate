187bd3bb130f9fbe1602763400b73694a68a5ecf
Lekshmi Chandra <lekshmi-c.sheela@moebel.de>
2020-11-17 20:55:50 +0100

    Revert "api integ"
    This reverts commit 1c1e6f6ce22013d6f58b009022baba34f653b7c9.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
 import * as React from "react";
-import List from "./List";
-import { QueryCache, ReactQueryCacheProvider } from "react-query";
-
-const queryCache = new QueryCache();
 
 export const AppComponent = () => {
-  return (
-    <ReactQueryCacheProvider queryCache={queryCache}>
-      <List></List>
-    </ReactQueryCacheProvider>
-  );
+  return <div>'Hello there!'</div>;
 };
